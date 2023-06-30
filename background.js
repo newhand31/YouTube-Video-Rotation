6ce23a1fabc6c41612bfc9d8d233a0bcf9c8f8ee
世明 王 <sam@paiyang.com.tw>
2023-06-30 09:57:53 +0800

    修改監聽事件

--- background.js
+++ background.js
@@ -1,13 +1,26 @@
 // background.js
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-        const queryParameters = tab.url.split("?")[1];
-        const urlParameters = new URLSearchParams(queryParameters);
+// chrome.tabs.onUpdated.addListener((tabId, tab) => {
+//     if (tab.url && tab.url.includes("youtube.com/watch")) {
+//         const queryParameters = tab.url.split("?")[1];
+//         const urlParameters = new URLSearchParams(queryParameters);
 
-        chrome.tabs.sendMessage(tabId, {
-            type: "NEW",
-            videoId: urlParameters.get("v"),
-        });
-    }
-});
+//         chrome.tabs.sendMessage(tabId, {
+//             type: "NEW",
+//             videoId: urlParameters.get("v"),
+//         });
+//     }
+// });
+chrome.webNavigation.onHistoryStateUpdated.addListener((details) => {
+    chrome.tabs.get(details.tabId, (tab) => {
+        if (tab.url && tab.url.includes("youtube.com/watch")) {
+            const queryParameters = tab.url.split("?")[1];
+            const urlParameters = new URLSearchParams(queryParameters);
+
+            chrome.tabs.sendMessage(tab.id, {
+                type: "NEW",
+                videoId: urlParameters.get("v"),
+            });
+        }
+    });
+}, { url: [{ hostSuffix: 'youtube.com' }] });
 
--- contentScript.js
+++ contentScript.js
@@ -18,7 +18,7 @@
         if (!bookmarkBtnExists) {
             const bookmarkBtn = document.createElement("img");
 
-            bookmarkBtn.src = chrome.runtime.getURL("assets/retation.png");
+            bookmarkBtn.src = chrome.runtime.getURL("assets/rotation.png");
             bookmarkBtn.className = "ytp-button " + "bookmark-btn";
             bookmarkBtn.title = "Click to bookmark current timestamp";
 
@@ -29,37 +29,35 @@
             bookmarkBtn.addEventListener("click", rotateVideo);
         }
     }
+    function rotateVideo() {
+        let video = document.querySelector('video.html5-main-video');
+        if (video) {
+            video.style.transformOrigin = 'center center';
+            video.style.transform += 'rotate(90deg)';
+            // adjustVideoSize(video);
+        }
+    }
+    document.addEventListener('DOMContentLoaded', newVideoLoaded);
 })();
 // contentScript.js
-function addButton() {
-    let container = document.querySelector('.ytp-left-controls');
-    if (container && !container.querySelector('.my-custom-button')) {
-        let button = document.createElement('button');
-        button.className = 'my-custom-button';
-        button.innerText = 'My Button';
-        container.appendChild(button);
-        button.addEventListener('click', rotateVideo);
-    }
-}
+// function addButton() {
+//     let container = document.querySelector('.ytp-left-controls');
+//     if (container && !container.querySelector('.my-custom-button')) {
+//         let button = document.createElement('button');
+//         button.className = 'my-custom-button';
+//         button.innerText = 'My Button';
+//         container.appendChild(button);
+//         button.addEventListener('click', rotateVideo);
+//     }
+// }
+// function adjustVideoSize(video) {
+//     const videoRect = video.getBoundingClientRect();
+//     const bodyRect = document.body.getBoundingClientRect();
+//     const windowWidth = window.innerWidth;
+//     const windowHeight = window.innerHeight;
+//     const scaleX = windowWidth / (videoRect.width + bodyRect.left);
+//     const scaleY = windowHeight / (videoRect.height + bodyRect.top);
+//     const scale = Math.min(scaleX, scaleY);
+//     video.style.transform += ` scale(${scale})`;
+// }
 
-function rotateVideo() {
-    let video = document.querySelector('video.html5-main-video');
-    if (video) {
-        video.style.transformOrigin = 'center center';
-        video.style.transform += 'rotate(90deg)';
-        // adjustVideoSize(video);
-    }
-}
-
-function adjustVideoSize(video) {
-    const videoRect = video.getBoundingClientRect();
-    const bodyRect = document.body.getBoundingClientRect();
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    const scaleX = windowWidth / (videoRect.width + bodyRect.left);
-    const scaleY = windowHeight / (videoRect.height + bodyRect.top);
-    const scale = Math.min(scaleX, scaleY);
-    video.style.transform += ` scale(${scale})`;
-}
-
-document.addEventListener('DOMContentLoaded', addButton);
